977504ca9eabf3554e83bf3f3fbc4442b7d07aa8
Brendan <brendansleung@gmail.com>
2023-05-20 20:41:23 -0700

    its transparent lol

--- gluchart-react/src/Components/JS/Form.js
+++ gluchart-react/src/Components/JS/Form.js
@@ -5,9 +5,52 @@ import Button2 from './Button2'
 
 export default function Form() {
     const [activeButton, setActiveButton] = useState('Glucose');
+    const [glucoseValue, setGlucoseValue] = useState('');
+    const [insulinValue, setInsulinValue] = useState('');
+    const [mealValue, setMealValue] = useState('');
 
     const handleButtonClick = (buttonText) => {
         setActiveButton(buttonText);
+    };
+
+    const handleInputChange = (event) => {
+        const { value } = event.target;
+        switch (activeButton) {
+          case 'Glucose':
+            setGlucoseValue(value);
+            break;
+          case 'Insulin':
+            setInsulinValue(value);
+            break;
+          case 'Meal':
+            setMealValue(value);
+            break;
+          default:
+            break;
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Access the variables (glucoseValue, insulinValue, mealValue) here based on the button pressed
+        console.log('Glucose:', glucoseValue);
+        console.log('Insulin:', insulinValue);
+        console.log('Meal:', mealValue);
+    
+        // Reset the input value
+        switch (activeButton) {
+          case 'Glucose':
+            setGlucoseValue('');
+            break;
+          case 'Insulin':
+            setInsulinValue('');
+            break;
+          case 'Meal':
+            setMealValue('');
+            break;
+          default:
+            break;
+        }
     };
 
     return (
@@ -38,8 +81,16 @@ export default function Form() {
 
             <hr className="divider"></hr>
 
-            <label htmlFor="fname" className="grams">insert grams</label>
-            <input type="text" id="fname" name="firstname" className="grams-input"></input>
+            <label htmlFor="input" className="grams">insert grams</label>
+            <input type="number" id="input" name="grams" className="grams-input" 
+                value={
+                    activeButton === 'Glucose'
+                        ? glucoseValue
+                        : activeButton === 'Insulin'
+                        ? insulinValue
+                        : mealValue
+                }
+            onChange={handleInputChange}></input>
 
             <div className="bottom-buttons">
                 <Button2 text="predict" color="var(--sage-green)" />
