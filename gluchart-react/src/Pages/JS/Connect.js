ed351a83e057aa677c01944b93f3f6b834e69398
SriMagesh <srirammagesh2@gmail.com>
2023-05-21 05:37:55 -0700

    Updated Connect page

--- gluchart-react/src/Pages/JS/Connect.js
+++ gluchart-react/src/Pages/JS/Connect.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import '../CSS/Home.css';
+import React, { useState } from 'react';
 import Hamburger from '../../menu.png';
-import Graph from '../../Components/JS/Graph';
-import Input from '../../Components/JS/Form';
+import '../CSS/Connect.css';
 import Navbar from '../../Components/JS/Navbar';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Connect = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="main-div">
-      <Navbar />
-      <div className="connect-page">
+      <div className={`home-div ${isMenuOpen ? 'menu-open' : ''}`}>
+        <div className="icon" onClick={handleMenuToggle}>
+          <img src={Hamburger} alt="Hamburger" />
+        </div>
+
         <h2>Connect</h2>
         <div className="connect-buttons">
           <button>Button 1</button>
@@ -17,8 +25,28 @@ const Home = () => {
           <button>Button 3</button>
         </div>
       </div>
+
+      {isMenuOpen && (
+        <div className="overlay" onClick={handleMenuToggle}>
+          <div className="navbar-overlay">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/connect">Connect</Link>
+              </li>
+              <li>
+                <Link to="/settings">Settings</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {isMenuOpen && <Navbar />}
     </div>
   );
 };
 
-export default Home;
+export default Connect;
