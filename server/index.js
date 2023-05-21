ddf000a816afe22bab398a3d147041a3c8dd2b6a
Tim Stewart <timstewartj@gmail.com>
2023-05-21 00:24:05 -0700

    try reading key from file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const api_key = require('/home/api_key.json')['key']
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + api_key)
+})
+
+app.post('/score', (req, res) => {
+    fetch("https://gluchartwrapper.azure-api.net/score",
+    {
+        method: "post",
+        headers: {
+            'Content-Type': "application/json",
+            'authorization': ('Bearer ' + api_key),
+            'azureml-model-deployment': 'glucode-defaults-model',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        },
+        body: req.body
+        })
+        .then((response) =>
+        {
+            res.send(response)
+        })
 })
 
 app.listen(port, () => {
