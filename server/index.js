801074def8d73d5cbd9f1e745241a21acf3ab0f8
Tim Stewart <timstewartj@gmail.com>
2023-05-21 02:05:11 -0700

    fix request sending

--- server/index.js
+++ server/index.js
@@ -3,28 +3,38 @@ const app = express()
 const port = process.env.PORT || 3001
 const api_key = require('/home/api_key.json')['key']
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/score', (req, res) => {
-    
+    console.log(req.body)
+    console.log(JSON.stringify(req.body))
     fetch("https://gluchartwrapper.azure-api.net/score",
     {
         method: "post",
         headers: {
             'Content-Type': "application/json",
             'authorization': ('Bearer ' + api_key),
-            'azureml-model-deployment': 'glucode-defaults-model',
+            'azureml-model-deployment': 'glucose-defaults-model',
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
         },
-        body: req.body
+        body: JSON.stringify(req.body)
         })
-        .then((response) => response.json())
-        .then((data) => {
-            return res.json({test: "joe mama", output: data})
-        })
+        .then((response) => {
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+            return response.json().then(data => {
+                return res.json({test: "joe mama", output: data})
+            });
+            } else {
+            return response.text().then(text => {
+                return res.send(text)
+            });
+        }})
 })
 
 app.listen(port, () => {
