b02b8d38f05023be81f80482fd71ec3b94ef07ee
SriMagesh <srirammagesh2@gmail.com>
2023-05-20 21:37:21 -0700

    Chart2!!

--- gluchart-react/src/Components/JS/Graph.js
+++ gluchart-react/src/Components/JS/Graph.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import '../CSS/Graph.css';
+import React, { useEffect, useRef } from "react";
+import "../CSS/Graph.css";
+
+// import { Line } from "react-chartjs-2";
+import ChartJS, {
+//   Chart as ChartJS,
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+} from "chart.js/auto";
+
+// ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 export default function Graph() {
-    return (
-        <div className="graph">
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-            <p>Graph</p>
-        </div>
-    );
+  const ref = useRef();
+  // const ctx = document.getElementById('myChart');
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const data = {
+      labels: ["May 12", "May 13", "May 14", "May 15", "May 16", "May 17"],
+      datasets: [
+        {
+          data: [8, 7.8, 6, 8, 7, 5, 6],
+        },
+      ],
+    };
+    const options = {};
+    const chart = new ChartJS(ref.current, {
+      type: "line",
+      data,
+      options,
+    });
+
+    return () => {
+        chart.destroy()
+      // remove the chart
+    };
+  }, [ref]);
+
+  return (
+    <div className="graph">
+      <canvas id="myChart" ref={ref} />
+      {/* <Line data={data} option = {options}></Line> */}
+    </div>
+  );
 }
